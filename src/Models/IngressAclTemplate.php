020676edb69b1a7abf1ee3b640656d62b4a6b9af
Fábio Santos <fsantos@ccsint.eu>
2018-11-15 14:06:39 +0000

    Add More Parameters to Ingress Acl Templates

--- src/Models/IngressAclTemplate.php
+++ src/Models/IngressAclTemplate.php
@@ -14,6 +14,9 @@ class IngressAclTemplate extends AbstractModel
 	public $defaultAllowIP;
 	public $defaultAllowNonIP;
 	public $active;
+	public $allowAddressSpoof;
+	public $priorityType;
+	public $description;
 
 	public function __construct(\Vsd\Common\Resources\Connection $client)
 	{
